a2e92cf0bcca0a8b3aff2f0b9bd2559619a206f5
Sergei <pavserger@gmail.com>
2024-07-27 08:51:36 +0300

    26.07.24 08:00 image ?

--- src/main/java/com/example/dacha/controllers/ViewParcel.java
+++ src/main/java/com/example/dacha/controllers/ViewParcel.java
@@ -4,8 +4,19 @@ import com.example.dacha.CrudService;
 import com.example.dacha.model.ParcelRepository;
 import com.example.dacha.model.PaysAndJobs.AnnualPaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+//import org.apache.commons.compress.utils.IOUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ViewParcel {
@@ -22,5 +33,32 @@ public class ViewParcel {
 
         return "viewparcel";
     }
+    /*
+    @GetMapping(value = "/image")
+    public @ResponseBody byte[] getImage() throws IOException {
+        InputStream in = getClass()
+                .getResourceAsStream("/com/baeldung/produceimage/image.jpg").;
+    //    ByteArrayOutputStream IOUtils = null;
+     //   return IOUtils.toByteArray(in);
+        return in;
+
+    }
+
+     */
+    @GetMapping("/get-image-dynamic-type")
+    @ResponseBody
+    public ResponseEntity<InputStreamResource> getImageDynamicType(@RequestParam("jpg") boolean jpg) {
+        MediaType contentType = jpg ? MediaType.IMAGE_JPEG : MediaType.IMAGE_PNG;
+        InputStream in = jpg ?
+                getClass().getResourceAsStream("/com/baeldung/produceimage/image.jpg") :
+                getClass().getResourceAsStream("/com/baeldung/produceimage/image.png");
+        return ResponseEntity.ok()
+                .contentType(contentType)
+                .body(new InputStreamResource(in));
+    }
+
+
+
+
 
 }
