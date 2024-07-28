f41230d9d94bd1159fa6688d299321a3036ca82b
Sergei <pavserger@gmail.com>
2024-07-28 07:21:47 +0300

    27.07.24 07:30 image ?

--- src/main/java/com/example/dacha/controllers/ViewParcel.java
+++ src/main/java/com/example/dacha/controllers/ViewParcel.java
@@ -3,20 +3,23 @@ package com.example.dacha.controllers;
 import com.example.dacha.CrudService;
 import com.example.dacha.model.ParcelRepository;
 import com.example.dacha.model.PaysAndJobs.AnnualPaymentRepository;
+import com.example.dacha.model.RollesAndUsers.Landowner;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 //import org.apache.commons.compress.utils.IOUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.List;
 
 @Controller
 public class ViewParcel {
@@ -45,13 +48,15 @@ public class ViewParcel {
     }
 
      */
-    @GetMapping("/get-image-dynamic-type")
-    @ResponseBody
+    @GetMapping("viewparcel/img")
+   // @ResponseBody
+    /*
     public ResponseEntity<InputStreamResource> getImageDynamicType(@RequestParam("jpg") boolean jpg) {
+        //public ResponseEntity<InputStreamResource> getImageDynamicType(@RequestParam("jpg") boolean jpg) {
         MediaType contentType = jpg ? MediaType.IMAGE_JPEG : MediaType.IMAGE_PNG;
         InputStream in = jpg ?
-                getClass().getResourceAsStream("/com/baeldung/produceimage/image.jpg") :
-                getClass().getResourceAsStream("/com/baeldung/produceimage/image.png");
+                getClass().getResourceAsStream("dacha2.png") :
+                getClass().getResourceAsStream("dacha2.jpg");
         return ResponseEntity.ok()
                 .contentType(contentType)
                 .body(new InputStreamResource(in));
@@ -59,6 +64,27 @@ public class ViewParcel {
 
 
 
+
+     */
+/*
+    public ResponseEntity<InputStreamResource> getImageDynamicType() {
+        //public ResponseEntity<InputStreamResource> getImageDynamicType(@RequestParam("jpg") boolean jpg) {
+      //  MediaType contentType = jpg ? MediaType.IMAGE_JPEG : MediaType.IMAGE_PNG;
+        InputStream in = getClass().getResourceAsStream("dacha2.png");
+        return new ResponseEntity<InputStreamResource>((MultiValueMap<String, String>) in, HttpStatus.OK);
+
+    }
+*/
+
+    public @ResponseBody byte[] getImage() throws IOException {
+
+ //       OutputStream os = new FileOutputStream("output.txt");
+ //       os.write(65);
+  //      InputStream in = getClass()
+  //              .getResourceAsStream("dacha2.PNG");
+        InputStream fileSystemStream = new FileInputStream("dacha2.PNG");
+        return IOUtils.toByteArray(fileSystemStream);
+    }
 
 
 }
